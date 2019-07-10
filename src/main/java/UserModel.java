f9c1113915a79074cadcc166f140bc289e242a34
Petar Petrov <PePetrov@objectsystems.com>
2019-07-10 18:27:35 +0300

    Initial commit

---
+++ src/main/java/UserModel.java
@@ -0,0 +1,103 @@
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * (enter description here)
+ * <p>
+ *
+ * @author PePetrov
+ * @author Copyright (c) 2019 MountainView Software, Corp.
+ */
+@DatabaseTable(tableName = "user")
+public class UserModel {
+
+  @DatabaseField(generatedId = true)
+  private int id;
+
+  @DatabaseField(canBeNull = false)
+  private String username;
+
+  @DatabaseField(canBeNull = false)
+  private String firstname;
+
+  @DatabaseField(canBeNull = false)
+  private String lastname;
+
+  @DatabaseField(canBeNull = false)
+  private String email;
+
+  @DatabaseField(canBeNull = false)
+  private String password_hash;
+
+  @DatabaseField(canBeNull = false)
+  private int access;
+
+  public UserModel() {
+
+  }
+
+  public UserModel(String username, String firstname, String lastname, String email, String password_hash, int access) {
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.password_hash = password_hash;
+    this.access = access;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword_hash() {
+    return password_hash;
+  }
+
+  public void setPassword_hash(String password_hash) {
+    this.password_hash = password_hash;
+  }
+
+  public int getAccess() {
+    return access;
+  }
+
+  public void setAccess(int access) {
+    this.access = access;
+  }
+}
